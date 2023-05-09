2c4422e8cb9aa502c66e1ca2af10dc51fabae256
Graham Downs <downs_graham@yahoo.com>
2023-05-09 13:52:04 +0200

    Create project and solution

---
+++ WeatherTooter/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherTooter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

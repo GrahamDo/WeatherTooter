68e67abd019425b35722bd09e0cc401404a844b1
Graham Downs <downs_graham@yahoo.com>
2023-05-09 14:36:03 +0200

    Add settings and enable setting things from command line

--- WeatherTooter/Program.cs
+++ WeatherTooter/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                var settings = Settings.Load();
+                if (args.Length == 3 && args[0].ToLower() == "--set")
+                {
+                    settings.SetValueFromArguments(args[1], args[2]);
+                    settings.Save();
+                    return;
+                }
+            }
+            catch (ApplicationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
---
+++ WeatherTooter/Settings.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json;
+
+namespace WeatherTooter
+{
+    internal class Settings
+    {
+        private const string SettingsFileName = "settings.json";
+        
+        public float LocationLatitude { get; set; }
+        public float LocationLongitude { get; set; }
+        public string LocationName { get; set; }
+        [JsonIgnore]
+        public string LocationNameNoSpaces => LocationName.Replace(" ", "");
+        public byte HoursToForecast { get; set; }
+        public string MastodonToken { get; set; }
+        public string MastodonInstanceUrl { get; set; }
+
+        public Settings()
+        {
+            LocationName = string.Empty;
+            MastodonToken = string.Empty;
+            MastodonInstanceUrl = string.Empty;
+        }
+
+        public static Settings Load()
+        {
+            if (!File.Exists(SettingsFileName))
+                return new Settings();
+
+            var text = File.ReadAllText(SettingsFileName);
+            return JsonConvert.DeserializeObject<Settings>(text) ??
+                   throw new ApplicationException($"Your '{SettingsFileName}' appears to be empty or corrupt.");
+        }
+
+        public void Save()
+        {
+            var serialised = JsonConvert.SerializeObject(this, Formatting.Indented);
+            File.WriteAllText(SettingsFileName, serialised);
+        }
+
+        public void SetValueFromArguments(string setting, string value)
+        {
+            try
+            {
+                switch (setting.ToLower())
+                {
+                    case "locationlatitude":
+                        LocationLatitude = float.Parse(value);
+                        break;
+                    case "locationlongitude":
+                        LocationLongitude = float.Parse(value);
+                        ;
+                        break;
+                    case "locationname":
+                        LocationName = value;
+                        break;
+                    case "hourstoforecast":
+                        HoursToForecast = byte.Parse(value);
+                        break;
+                    case "mastodontoken":
+                        MastodonToken = value;
+                        break;
+                    case "mastodoninstanceurl":
+                        MastodonInstanceUrl = value;
+                        break;
+                    default:
+                        throw new ApplicationException($"Invalid setting: {setting}");
+                }
+            }
+            catch (FormatException ex)
+            {
+                throw new ApplicationException($"{value} is an invalid value for {setting}");
+            }
+        }
+    }
+}
